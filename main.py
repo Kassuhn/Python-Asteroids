9b8dac0adbfedc9d697cb945710af32163b078e9
Kassuhn <Rhyankassuhn@gmail.com>
2026-03-16 10:39:54 -0700

    C: player object created with triangle shape. Circle hitbox created.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,6 +13,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -19,10 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)
-        dt = dt / 1000
-        
+        dt = dt / 1000        
         
 
 if __name__ == "__main__":
