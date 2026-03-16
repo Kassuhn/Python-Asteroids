32c14463f2f8012752e56deffd860cdda709d453
Kassuhn <Rhyankassuhn@gmail.com>
2026-03-16 10:13:56 -0700

    B: FPS set in main() and CircleShape class created

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +20,10 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)
+        dt = dt / 1000
+        
+        
 
 if __name__ == "__main__":
     main()
